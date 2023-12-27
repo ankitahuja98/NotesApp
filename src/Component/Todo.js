dcb25dcd39c41fa94373464e7d9ac5219232d1a9
ankitahuja98 <144571615+ankitahuja98@users.noreply.github.com>
2023-12-28 03:39:20 +0530

    add items and delelte items functionality done

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import headericon from '../Images/Todo_icon.png'
+import Tooltip from '@mui/material/Tooltip';
+
 
 const Todo = () => {
+    const [input, setinput] = useState("");
+    const [additm, setadditm] = useState([]);
+
+    // function Capitalize(str) {
+    //     return str.charAt(0).toUpperCase() + str.slice(1);
+    // }
+
+    const inputChange = (event) => {
+        setinput(event.target.value)
+    }
+
+    const additem = () => {
+        if (input === "") {
+            return 0;
+        } else {
+            setadditm([input, ...additm]);
+            setinput("");
+        }
+    }
+    // delete item function
+    // onclick event pe ek function call kia hai jo notes ki index leke ayega yaha per 
+    // yha id mai vo item ki index store hogi.
+    const deleteItem = (id) => {
+        // id vo niche se leke aa rha hai
+        //or index additem mai jo bhi array hai unka index id bn gya
+        const dltitemarray = additm.filter((value, index) => {
+            return (index !== id);
+        })
+        setadditm(dltitemarray)
+    }
+
+    const deleteAll = () => {
+        setadditm([])
+    }
     return (
         <div className='todo_body'>
 
@@ -16,8 +52,8 @@ const Todo = () => {
             <div className="container">
                 <div className="d-flex justify-content-center">
                     <div className='inputDiv d-flex align-items-center'>
-                        <input type='text' placeholder='Enter the List Item' />
-                        <i className="fa-solid fa-plus" />
+                        <input type='text' placeholder='Enter the List Item' value={input} onChange={inputChange} />
+                        <i className="fa-solid fa-plus" onClick={additem} />
                     </div>
                 </div>
             </div>
@@ -36,6 +72,11 @@ const Todo = () => {
                         </div>
                     </div>
                 </div>
+                <div className='row'>
+                    <div className="col-12">
+                        <button className='btn btn-danger my-4' onClick={deleteAll}>Delete All</button>
+                    </div>
+                </div>
             </div>
             {/* display list heading ends */}
 
@@ -45,18 +86,30 @@ const Todo = () => {
                 <div className="row">
 
                     {/* list start */}
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className='show_list'>
-                            <div>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eos soluta ullam quam sint cumque culpa ut eum quae illo, illum iure doloribus mollitia hic dolores sequi unde nobis asperiores iste veritatis, atque nisi tempora debitis fugiat. Et omnis, cum architecto dolorum dolore veniam nulla, consequuntur voluptates, consequatur provident fuga?</p>
-                            </div>
-                            <i className="fa-solid fa-check"></i>
-                            <i className="fa-solid fa-trash"></i>
-                            <i className="fa-solid fa-pen-to-square"></i>
-                        </div>
-                    </div>
-                    {/* list ends */}
+                    {additm.map((value, index) => {
+                        return (
 
+                            <div className="col-12 col-md-6 col-lg-4" key={index} >
+
+                                <div className='show_list'>
+                                    <div>
+                                        <p className='showitem_p'>{value}</p>
+                                    </div>
+                                    <Tooltip title="Mark as Completed">
+                                        <i className="fa-solid fa-check"></i>
+                                    </Tooltip>
+                                    <Tooltip title="Delete">
+                                        <i className="fa-solid fa-trash" onClick={() => deleteItem(index)}></i>
+                                    </Tooltip>
+                                    <Tooltip title="Edit">
+                                        <i className="fa-solid fa-pen-to-square"></i>
+                                    </Tooltip>
+                                </div>
+                            </div>
+
+                        )
+                    })}
+                    {/* list ends */}
 
                 </div>
             </div>
